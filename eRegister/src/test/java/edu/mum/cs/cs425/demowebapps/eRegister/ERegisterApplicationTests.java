f42275ecdcbc02ceb7fa873c941c5ba28c253f99
shimulgithub <87926468+shimulgithub@users.noreply.github.com>
2023-09-03 20:09:08 -0500

    Add files via upload

---
+++ eRegister/src/test/java/edu/mum/cs/cs425/demowebapps/eRegister/ERegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.eRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
